43a7b857797ec85374f2acbb5c016d8961d1f61d
GengStore <127430664+SegengKganz@users.noreply.github.com>
2023-07-01 23:16:49 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.creator = 'GengStoreOfficial'// yourname
+global.MONGO_DB_URI = "mongodb+srv://segengkpanel:<password>@cluster0.iv0sf1p.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10837715" //isi apa aja bebas
+global.your_email = "segengelek@gmail.com" //email
+global.email_password = "crubdmyptfjriagc" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
